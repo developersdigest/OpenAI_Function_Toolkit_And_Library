b434ba58c363b8161c8c29c3b5a6f25037c8b42f
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-06-29 07:23:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { ai_functions } from "./utils/openai-functions-toolkit.js";
 
 const funcA = new ai_functions("Open the calculator on my computer");
-const resB = await funcA.call();
-console.log(resB);
-
-const funcB = new ai_functions("Take a screenshot of Amazon.com");
-const resA = await funcB.call();
+const resA = await funcA.call();
 console.log(resA);
 
+const funcB = new ai_functions("Take a screenshot of Amazon.com");
+const resB = await funcB.call();
+console.log(resB);
+
 const funcC = new ai_functions("Scrape the website https://www.google.com and take a screenshot", {
+  // Limit the functions to be used and passed to OpenAI with the optional functionalArray setting
   functionArray: ["scrapeWebsite", "takeScreenshot"]
 });
 const resC = await funcC.call();
