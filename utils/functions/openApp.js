9542ae2c0717fb56fa8aba6ff8b2f62cdd913b72
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-06-29 07:24:26 -0400

    Update openApp.js

--- utils/functions/openApp.js
+++ utils/functions/openApp.js
@@ -1,34 +1,28 @@
 import { exec } from 'child_process';
-
-// Declare the execute function
-async function execute({ appName }) {
-  return new Promise((resolve, reject) => {
-    exec(`open -a "${appName}"`, (error, stdout, stderr) => {
-      if (error) {
-        console.warn(error);
-        reject(`Error opening ${appName}: ${error.message}`);
-      }
-      resolve(`${appName} opened successfully.`);
+export const execute = (options) => {
+    const { appName } = options;
+    return new Promise((resolve, reject) => {
+        exec(`open -a "${appName}"`, (error, stdout, stderr) => {
+            if (error) {
+                console.warn(error);
+                reject(`Error opening ${appName}: ${error.message}`);
+            }
+            resolve(`${appName} opened successfully.`);
+        });
     });
-  });
 }
-
-// Export the function and its details
-export const openApp = {
-  execute,
-  details: {
+export const details = {
     name: "openApp",
     description: "Opens a specified application on your computer",
     parameters: {
-      type: "object",
-      properties: {
-        appName: {
-          type: "string",
-          description: "The name of the application to open"
+        type: "object",
+        properties: {
+            appName: {
+                type: "string",
+                description: "The name of the application to open"
+            },
         },
-      },
-      required: ["appName"],
+        required: ["appName"],
     },
-  },
-  example: "Open the 'Calculator' application"
+    example: "Open the 'Calculator' application"
 };
