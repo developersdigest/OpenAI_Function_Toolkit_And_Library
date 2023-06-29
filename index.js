cedb4902a281289fe63e7a81545e43a2421e6217
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-06-29 06:30:30 -0400

    Update index.js to be in a functional vs oop

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import { ai_functions } from "./utils/openai-functions-toolkit.js";
 
-// 1. Take a screenshot of a website
-ai_functions({message: "Take a screenshot of Amazon.com",});
+const funcA = new ai_functions("Open the calculator on my computer");
+const resB = await funcA.call();
+console.log(resB);
 
-// // 2. Scrape a website and take a screenshot
-ai_functions({
-  message: "Scrape the website https://www.google.com and take a screenshot",
-  functionArray: ["scrapeWebsite", "takeScreenshot"],
+const funcB = new ai_functions("Take a screenshot of Amazon.com");
+const resA = await funcB.call();
+console.log(resA);
+
+const funcC = new ai_functions("Scrape the website https://www.google.com and take a screenshot", {
+  functionArray: ["scrapeWebsite", "takeScreenshot"]
 });
-
-// 3. Open the calculator app
-ai_functions({ message: "Open the calculator app on my computer" });
-
-// 4. Open system preferences
-ai_functions({ message: "Open the activity monitor on my computer" });
+const resC = await funcC.call();
+console.log(resC);
