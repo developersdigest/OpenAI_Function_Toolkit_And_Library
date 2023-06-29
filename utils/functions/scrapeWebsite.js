7dbb286e07dde30a89c00fdf977a256e2aa3d33a
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-06-29 07:24:38 -0400

    Update scrapeWebsite.js

--- utils/functions/scrapeWebsite.js
+++ utils/functions/scrapeWebsite.js
@@ -1,20 +1,17 @@
 import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
 import fs from "fs";
 import path from "path";
-
-async function execute({ url }) {
+export const execute = async (options) => {
+  const { url } = options;
   try {
     const websiteName = new URL(url).hostname;
     const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-
     const outputDirectory = path.join(".", "output");
     if (!fs.existsSync(outputDirectory)) {
       fs.mkdirSync(outputDirectory, { recursive: true });
     }
-
     const saveFileName = `${websiteName}_${timestamp}.html`;
     const saveFilePathWithDate = path.join(outputDirectory, saveFileName);
-
     const loader = new PuppeteerWebBaseLoader(url, {
       launchOptions: {
         headless: 'new',
@@ -30,19 +27,14 @@ async function execute({ url }) {
         return `Website HTML saved to ${saveFilePathWithDate}`;
       },
     });
-
     await loader.load();
-
     return `Website HTML saved to ${saveFilePathWithDate}`;
   } catch (error) {
     console.error("Error occurred while scraping HTML:", error);
     throw error;
   }
-}
-
-export const scrapeWebsite = {
-  execute,
-  details: {
+};
+export const details = {
     name: "scrapeWebsite",
     description: "Scrape the HTML of a website",
     parameters: {
@@ -55,6 +47,5 @@ export const scrapeWebsite = {
       },
       required: ["url"],
     },
-  },
-  example: "Scrape the HTML for Google"
+    example: "Scrape the HTML for Google"
 };
