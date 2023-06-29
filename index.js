39bf531fc0fe14235a5111f644e765267e3a68a3
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-06-28 21:37:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { ai_functions } from "./utils/openai-functions-toolkit.js";
 
 // 1. Take a screenshot of a website
-// ai_functions({message: "Take a screenshot of Amazon.com",});
+ai_functions({message: "Take a screenshot of Amazon.com",});
 
 // // 2. Scrape a website and take a screenshot
-// ai_functions({
-//   message: "Scrape the website https://www.google.com and take a screenshot",
-//   functionArray: ["scrapeWebsite", "takeScreenshot"],
-// });
+ai_functions({
+  message: "Scrape the website https://www.google.com and take a screenshot",
+  functionArray: ["scrapeWebsite", "takeScreenshot"],
+});
 
 // 3. Open the calculator app
 ai_functions({ message: "Open the calculator app on my computer" });
